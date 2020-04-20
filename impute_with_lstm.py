017a890fe36bef58663986c8fddfa0c878b60d54
Sreya Ghosh <sreyag28@gmail.com>
2020-04-20 00:15:41 -0700

    Update impute_with_lstm.py

--- impute_with_lstm.py
+++ impute_with_lstm.py
@@ -1,63 +1,101 @@
+
 """
 
-WHAT: [Purpose of code]
+WHAT: 
+This code imputes missing data using LSTM. 
+The data-set is cleaned and partitioned into training and testing data. 
+The model is trained using an LSTM with masking to account for missing data.
+The model is used to predict the missing data points.
 
-WHY: [Need that the code fulfills]
+WHY: 
+This code imputes and returns the complete data.  
 
-ASSUMES: [Files needed, libraries required]
+ASSUMES: 
+The requirements file is included. 
 
-FUTURE IMPROVEMENTS: [Optimizations, scaling issues, concerns]
+FUTURE IMPROVEMENTS: 
 
-VARIABLES: [Name and description of complex variables expected]
+The data-set is large and was truncated to 500,000 data points for training on a local computer. 
+Optimization of the data loading can improve the efficiency of the code, and allow training with more data improving the model (typical of NN).
+
+The choice of parameters in the model was searched over a small grid. Extensive hyper-parameter search. 
+
+
+VARIABLES: 
+df : Pandas dataframe with the entire data-set
+df_model : Pandas dataframe with the entire data-set with the features only
+utils : Utility functions included 
+scaler: Function to scale, and descale the features to improve modelling
+model : Keras based LSTM model with masking. 4 hidden layers and 2 fully connected layers
+10% of the data is used for cross-validation
+df_fin : Imputed Data
+Note: The MSE calculated for the test dataset occurs after re-scaling of the features and is larger than the validation/training data.
+It's not necessarily an effect of over-fitting.
 
 SG 2020/04/20
 
-
-
-OUTPUT: The input dataset with missing values filled.
-
+OUTPUT : Saves the imputed data file as .csv in the working directory.
 """
 
 
-
-# Import statements
-
+import argparse
 import pandas as pd
-import numpy as np
-import seaborn as sns
-from sklearn.metrics import mean_squared_error
+import sys
+import os
+import utils
 from sklearn.preprocessing import MinMaxScaler
-from matplotlib import pyplot
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import Flatten
-from keras.layers import Masking
-from keras.layers.core import Dense, Activation  
-from keras.layers.recurrent import LSTM
-from keras.callbacks import EarlyStopping
-import utils as utils
 
-def SampleFunc(var1, var2):
-
-    """
-
-    class/function definitions
-
-    [Short description of function]
-
-    [var1 - description]
-
-    [var2 - description]
-
-    [returns - description]
-
-    """
-
-    return
-
-
-
-# Test Code. Will run when this script is executed
+def main():
+  parser = argparse.ArgumentParser(description='File Path and/or Features')
+  parser.add_argument('path',metavar='path',type=str,help='the path to the dataset')
+  parser.add_argument('--features',metavar='features', type=str, help='Features to impute')
+  args = parser.parse_args()
+  
+  #path to the .csv data-set
+  input_path = args.path
+  try:
+    df = pd.read_csv(input_path,sep=";")
+  except:
+    sys.exit('Path does not exist')
+  print("The first 5 rows of the data-set")
+  print(df.head())
+  
+  #features to use 
+  if args.features:
+    cols = args.features.split(",")
+  else:
+    cols=df.columns.tolist()[2:]
+  print(cols)
+  for feature in cols:
+    if feature not in df.columns.tolist():
+      print('These features are not present')
+      sys.exit()
+  
+  df = utils.preprocessing(df,cols)
+  df_model = df.copy()
+  
+  #scale the data-set
+  scaler = MinMaxScaler()
+  df_model[cols] = scaler.fit_transform(df_model[cols].values)
+  
+  #data preparation
+  (X_train, y_train), (X_test, y_test) = utils.train_test_split(df_model.fillna(-1).head(500000))
+  X_missing,missing_indices = utils.find_missing_indices(df_model.head())
+  
+  #fitting the LSTM
+  model = utils.fit_model(X_train,y_train)
+  
+  #Test set
+  y_pred = scaler.inverse_transform(utils.predict_model(model,X_test))
+  mse = utils.evaluate(y_pred,scaler.inverse_transform(y_test))
+  
+  #Filling the missing data
+  y_missing = scaler.inverse_transform(utils.predict_model(model,X_missing))
+  df_fin=utils.filled_dataset(df,y_missing,missing_indices,cols)
+  print("Imputed missing data")
+  print(df_fin.iloc[missing_indices])
+  
+  df_fin.to_csv("impute_household_power_consumption.csv")
 
 if __name__ == '__main__':
   main()
